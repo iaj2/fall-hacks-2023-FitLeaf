fc84564d6551484a71b2d025e6af1cce14eb0125
iaj2 <isaacjames124@yahoo.ca>
2023-10-21 16:00:00 -0700

    client better now

--- app/src/levelSection.jsx
+++ app/src/levelSection.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import "./sass/levelSection.scss"
+import LeafIcon from './assets/leaf-icon.png'
 
 function LevelSection() {
- const getMessage = () => {
-    return "FitLeaf";
-  };
+
 
   return (
-    <div>
-      <h1 className="header">{getMessage()}</h1>
-      <h2>Become fit with every turn.</h2>
-      <h3>What's your fitness level?</h3>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button className="button" style={{ marginRight: '500px' }}>Beginner</button>
-        <button className="button" style={{ marginRight: '-280px' }}>Intermediate</button>
-        <button className="button" style={{ marginRight: '-1060px' }}>Advanced</button>
+    <div className='level-section-container'>
+      <div className='header-wrapper'>
+        <h1 className="fitleaf-header">FitLeaf <img src={LeafIcon} alt=''></img></h1>
+      </div>
+      <div className='tagline-wrapper'>
+        <h2 className='fitleaf-tagline'>Fitter with every turn.</h2>
+      </div>
+     <div className='level-header-wrapper'>
+      <h3 className='fitness-level-header'>What's your fitness level?</h3>
+     </div>
+      
+      <div className='buttons-container'>
+        <div className='buttons-wrapper'>
+        <button className="beginner-button" >Beginner</button>
+        <button className="intermediate-button" >Intermediate</button>
+        <button className="advanced-button">Advanced</button>
+        </div>
+        
       </div>
     </div>
   );
