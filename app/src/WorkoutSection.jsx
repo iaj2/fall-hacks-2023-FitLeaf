27b63e9e4604c246c9c07f1ce8d723280d262253
Leo Zhi <leozhi6869@gmail.com>
2023-10-21 15:50:41 -0700

    cursor became a leaf

--- app/src/WorkoutSection.jsx
+++ app/src/WorkoutSection.jsx
@@ -7,16 +7,18 @@ function WorkoutSection() {
   };
 
   return (
-    <div>
-      <h1 className="header">{getMessage()}</h1>
-      <h2>Become fit with every turn.</h2>
-      <h3>What's your fitness level?</h3>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button className="button" style={{ marginRight: '500px' }}>Beginner</button>
-        <button className="button" style={{ marginRight: '-280px' }}>Intermediate</button>
-        <button className="button" style={{ marginRight: '-1060px' }}>Advanced</button>
-      </div>
-    </div>
+    <body class='custom-cursor'>
+        <div>
+          <h1 className="header">{getMessage()}</h1>
+          <h2>Become fit with every turn.</h2>
+          <h3>What's your fitness level?</h3>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button className="button" style={{ marginRight: '500px' }}>Beginner</button>
+            <button className="button" style={{ marginRight: '-280px' }}>Intermediate</button>
+            <button className="button" style={{ marginRight: '-1060px' }}>Advanced</button>
+          </div>
+        </div>
+    </body>
   );
 }
 
